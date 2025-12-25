bad253258765cbae337ffa9416e0019d810afb2e
taishikato <taishi.k0903@gmail.com>
2025-12-24 20:42:17 -0500

    chore: remove legacy devtools package and dead code
    - Remove packages/devtools/ (replaced by shadcn-style templates)
    - Remove unused devtools-package.ts and previewcn-setup.ts utilities

--- packages/devtools/src/presets/index.ts
+++
@@ -1,4 +0,0 @@
-// Re-export all presets
-export { colorPresets, getColorPreset, type ColorPreset } from "./colors";
-export { radiusPresets, getRadiusPreset, type RadiusPreset } from "./radius";
-export { fontPresets, getFontPreset, type FontPreset } from "./fonts";
